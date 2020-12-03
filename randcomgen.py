bb7a6bde0b2712e193d37bd8b0c341ad3831f3e2
james_rush <jamesyhh@gmail.com>
2020-12-03 22:51:18 +0800

    remove last line LF

--- randcomgen.py
+++ randcomgen.py
@@ -8,13 +8,13 @@ counter = 0
 looptime = int(input('looptime:'))
 letter = string.ascii_letters + string.digits
 with open('./test.input', 'w+') as f:
-	while counter < looptime:
+	while counter < looptime - 1:
 		rcomflag = random.randint(0,2)
 		value = ''
 		if(rcomflag == 0):
 			key = random.randint(minint,maxint)
 			for i in range(128):
-				value = value + choice(letter)
+				value = value + random.choice(letter)
 			content = 'PUT ' + str(key) + ' ' + value
 		elif(rcomflag == 1):
 			key = random.randint(minint,maxint)
@@ -27,4 +27,21 @@ with open('./test.input', 'w+') as f:
 		if counter % 1000000 == 0:
 			print(counter)
 		counter = counter + 1
+
+	#last line no need \n
+	rcomflag = random.randint(0,2)
+	value = ''
+	if(rcomflag == 0):
+		key = random.randint(minint,maxint)
+		for i in range(128):
+			value = value + random.choice(letter)
+		content = 'PUT ' + str(key) + ' ' + value
+	elif(rcomflag == 1):
+		key = random.randint(minint,maxint)
+		content = 'GET ' + str(key)
+	elif(rcomflag == 2):
+		key1 = random.randint(minint,maxint - 100)
+		key2 = key1 + random.randint(1,100)
+		content = 'SCAN ' + str(key1) + ' ' + str(key2)
+	f.write(str(content))
 f.close()
